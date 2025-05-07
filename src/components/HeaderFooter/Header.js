7cd6541f9846288a97ab348577eb9ddcf29102fd
devavology <145644105+devavology@users.noreply.github.com>
2025-05-07 19:55:17 +0530

    add

--- src/components/HeaderFooter/Header.js
+++ src/components/HeaderFooter/Header.js
@@ -9,15 +9,17 @@ function Header() {
   <div className="container">
     <div className="grid">
       <div className="logos">
+      <Link href="/">
         <img src="images/logo.svg" alt="logo" />
+        </Link> 
       </div>
       <div className="link-bar">
          <ul>
           <li>
-            <Link href="/"></Link> 
+           <Link href="/index"></Link>
           </li>
           <li>
-            <Link href="/">Blog</Link> 
+          <Link href="/blog">Blog</Link> 
           </li>
           <li>
             <Link className="header-btn" href="/">Join the Community</Link> 
---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function blog() {
+  return (
+    <div>
+      tshytjhydtj
+    </div>
+  )
+}
+
+export default blog
