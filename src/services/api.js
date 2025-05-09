0cdbba3607bcffc8370a47e3bee0b2617d93a9bb
dev-avology <devavology12@gmail.com>
2025-05-09 18:37:52 +0530

    fix: strapi cros issues

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,11 @@ import axios from 'axios'
 
 const API = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
 });
 
 export default API;
