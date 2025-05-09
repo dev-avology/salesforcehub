d30f90e92aab79a6078ec75b7a1458dd51ddb91e
dev-avology <devavology12@gmail.com>
2025-05-09 19:27:09 +0530

    update: strapi upload location

--- backend/cms/config/plugins.ts
+++ backend/cms/config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default {
+  'upload': {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: 100000,
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  'upload-plugin': {
+    config: {
+      serverUrl: 'https://phpstack-1180784-5492094.cloudwaysapps.com',
+    },
+  },
+};
--- backend/cms/config/server.ts
+++ backend/cms/config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 3002),
+  url: env('PUBLIC_URL', 'https://phpstack-1180784-5492094.cloudwaysapps.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
